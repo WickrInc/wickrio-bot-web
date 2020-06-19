11a586d7298f4822f9e20d52545c160c96366109
alex@aml.codes <alex@aml.codes>
2020-06-19 15:01:17 -0400

    report table

--- src/components/message/report.js
+++ src/components/message/report.js
@@ -1,11 +1,215 @@
 import React, { useState, useContext, useEffect } from "react"
 import { Link } from "gatsby"
 import { MessageContext } from "../context"
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+
+
+const Row = ({ user }, key) => {
+  const { downloadReport, token, secGroups } = useContext(MessageContext)
+
+  const useRowStyles = makeStyles({
+    root: {
+      boxShadow: 'none',
+      '& > *': {
+        borderBottom: 'unset',
+      }
+    },
+  });
+  const classes = useRowStyles();
+  const [open, setOpen] = useState(false)
+
+  return (
+    <TableRow className={classes.root} key={key}>
+      {/* <TableCell>
+        <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+          {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </IconButton>
+      </TableCell> */}
+      <TableCell align="center">
+        <p className="sentmessagereport">
+          {user.status}
+        </p>
+      </TableCell>
+      <TableCell align="center">
+        <p className="sentmessagereport">
+          {user.user}
+        </p>
+      </TableCell>
+
+      <TableCell align="center">
+        {
+          user?.status_message?.includes('http') ?
+            <a href={user.status_message} rel="noopener noreferrer" target="_blank">
+              Location
+          </a> :
+            user.status_message}
+
+      </TableCell>
+      <TableCell align="center">
+        <div>
+          <p style={{
+            fontSize: 12,
+            fontFamily: 'Open Sans',
+            letterSpacing: '0.41px',
+            textAlign: 'right',
+            color: 'var(--text-light)',
+            lineHeight: 1.33
+          }}>
+            {new Date(user.read_datetime).toLocaleDateString()}
+          </p>
+
+          <p style={{
+            fontSize: 12,
+            fontFamily: 'Open Sans',
+            letterSpacing: '0.41px',
+            textAlign: 'right',
+            color: 'var(--text-light)',
+            lineHeight: 1.33
+          }}>
+            {new Date(user.read_datetime).toLocaleTimeString()}
+          </p>
+        </div>
+      </TableCell>
+      <TableCell align="center">
+        <div>
+          <p style={{
+            fontSize: 12,
+            fontFamily: 'Open Sans',
+            letterSpacing: '0.41px',
+            textAlign: 'right',
+            color: 'var(--text-light)',
+            lineHeight: 1.33
+          }}>
+            {new Date(user.read_datetime).toLocaleDateString()}
+          </p>
+
+          <p style={{
+            fontSize: 12,
+            fontFamily: 'Open Sans',
+            letterSpacing: '0.41px',
+            textAlign: 'right',
+            color: 'var(--text-light)',
+            lineHeight: 1.33
+          }}>
+            {new Date(user.read_datetime).toLocaleTimeString()}
+          </p>
+        </div></TableCell>
+    </TableRow>
+  )
+}
+
+
+
 
 const Report = ({ id }) => {
-  const { report, secGroups, sendReportStatus, downloadReport } = useContext(MessageContext)
+  const { report, secGroups, sendReportStatus, downloadReport, sentBroadcasts, sendStatus } = useContext(MessageContext)
   const [page, setPage] = useState(0)
   const [size, setSize] = useState(25)
+  const [order, setOrder] = React.useState('desc');
+  const [orderBy, setOrderBy] = React.useState('when_sent');
+  const [selected, setSelected] = React.useState([]);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(25);
+
+  const useRowStyles = makeStyles({
+    root: {
+      boxShadow: 'none',
+      maxHeight: 800,
+      '& > *': {
+        borderBottom: 'unset',
+      }
+    },
+    theader: {
+      backgroundColor: '#fff',
+      fontFamily: 'Open Sans',
+      fontSize: '14px',
+      fontWeight: 600,
+      lineHeight: 1.14,
+      letterSpacing: '1.28px',
+      // textAlign: right;
+      color: 'var(--text-light)'
+
+    }
+  });
+  const classes = useRowStyles();
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    console.log({ orderBy, property, order, isAsc })
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  useEffect(() => {
+    sendStatus(0, 25)
+    // sendBroadcastSummary()
+  }, [])
+
+  const getNextReportPage = (page, size) => {
+    sendStatus(page + 1, size)
+    setPage(page + 1)
+  }
+
+  const getLastReportPage = (page, size) => {
+    if (page !== 0) {
+      sendStatus(page - 1, size)
+      setPage(page - 1)
+
+      // console.log({ newpage: page - 1 })
+      // sendStatus(page, size)
+    }
+  }
+  let from = page == 0 ?
+    (sentBroadcasts.list.length - (sentBroadcasts.list.length - 1)) * page + 1 :
+    size * (sentBroadcasts.list.length - (sentBroadcasts.list.length - 1)) + 1
+
+
+  let to = sentBroadcasts.list.length * (page + 1)
+
+
+  const createSortHandler = (property) => (event) => {
+    handleRequestSort(event, property);
+  };
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, sentBroadcasts.list.length - page * rowsPerPage);
 
   console.log({ id })
 
@@ -21,17 +225,7 @@ const Report = ({ id }) => {
   // console.log({ secGroups, report, date })
   // console.log(report.broadcast.summary)
 
-  const getNextReportPage = (id, page, size) => {
-    // setPage(page + 1)
-    sendReportStatus(id, page, size)
-  }
-
-  const getLastReportPage = (id, page, size) => {
-    if (page !== 0) {
-      setPage(page - 1)
-      sendReportStatus(id, page, size)
-    }
-  }
+  console.log({ report })
 
 
   return (
@@ -115,99 +309,62 @@ const Report = ({ id }) => {
               overflow: 'scroll'
             }}
           >
-            <table >
-              <thead bgcolor="white">
-                <tr>
-                  <th className="Date">Status</th>
-                  <th className="tlabel">Name</th>
-                  <th className="tlabel">Message</th>
-                  <th className="tlabel">Sent</th>
-                  <th className="tlabel">Read</th>
-                </tr>
-              </thead>
-              <tbody>
-                {report.broadcast.report?.map((user, idx) => {
-                  if (user.status == 0) { user.status = "pending" }
-                  else if (user.status == 1) { user.status = "sent" }
-                  else if (user.status == 2) { user.status = "failed" }
-                  else if (user.status == 3) { user.status = "ack" }
-                  else if (user.status == 4) { user.status = "ignored" }
-                  else if (user.status == 5) { user.status = "aborted" }
-                  else if (user.status == 6) { user.status = "read" }
-                  else if (user.status == 7) { user.status = "delivered" }
 
-                  return (
-                    <tr key={idx}>
-                      <td>
-                        <p className="sentmessagereport">
-                          {user.status}
-                        </p>
-                      </td>
-                      <td className="trowreport">{user.user}</td>
-                      {user?.status_message?.includes('http') ?
-                        <td className="trowreport">
-                          <a href={user.status_message} rel="noopener noreferrer" target="_blank">
-                            Location
-                          </a>
-                        </td>
-                        :
-                        <td className="trowreport">
-                          {user.status_message}
-                        </td>
+            <TableContainer component={Paper} className={classes.root}>
+              <Table stickyHeader aria-label="sticky collapsible table">
+                <TableHead >
+                  <TableRow >
 
-                      }
-                      <td className="trowreport"><div>
-                        <p style={{
-                          fontSize: 12,
-                          fontFamily: 'Open Sans',
-                          letterSpacing: '0.41px',
-                          textAlign: 'right',
-                          color: 'var(--text-light)',
-                          lineHeight: 1.33
-                        }}>
-                          {new Date(user.sent_datetime).toLocaleDateString()}
-                        </p>
+                    <TableCell
+                      className={classes.theader}
+                    // align={headCell.numeric ? 'right' : 'left'}
+                    // padding={headCell.disablePadding ? 'none' : 'default'}
+                    >
+                      <TableSortLabel
+                        active={orderBy === 'when_sent'}
+                        direction={orderBy === 'when_sent' ? order : 'asc'}
+                        onClick={createSortHandler('when_sent')}
+                      >
+                        Status
+                        </TableSortLabel>
+                    </TableCell>
+                    <TableCell
+                      className={classes.theader}
+                      align="center">Name</TableCell>
+                    <TableCell
+                      className={classes.theader}
+                      align="center">Message</TableCell>
+                    <TableCell
+                      className={classes.theader}
+                      align="center">Sent</TableCell>
+                    <TableCell
+                      className={classes.theader}
+                      align="center">Read</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
 
-                        <p style={{
-                          fontSize: 12,
-                          fontFamily: 'Open Sans',
-                          letterSpacing: '0.41px',
-                          textAlign: 'right',
-                          color: 'var(--text-light)',
-                          lineHeight: 1.33
-                        }}>
-                          {new Date(user.sent_datetime).toLocaleTimeString()}
-                        </p>
-                      </div></td>
-                      <td className="trowreport"><div>
-                        <p style={{
-                          fontSize: 12,
-                          fontFamily: 'Open Sans',
-                          letterSpacing: '0.41px',
-                          textAlign: 'right',
-                          color: 'var(--text-light)',
-                          lineHeight: 1.33
-                        }}>
-                          {new Date(user.read_datetime).toLocaleDateString()}
-                        </p>
+                  {report.broadcast.report?.map((user, idx) => {
+                    if (user.status == 0) { user.status = "pending" }
+                    else if (user.status == 1) { user.status = "sent" }
+                    else if (user.status == 2) { user.status = "failed" }
+                    else if (user.status == 3) { user.status = "ack" }
+                    else if (user.status == 4) { user.status = "ignored" }
+                    else if (user.status == 5) { user.status = "aborted" }
+                    else if (user.status == 6) { user.status = "read" }
+                    else if (user.status == 7) { user.status = "delivered" }
+                    console.log({ user })
+                    return (
+                      <Row user={user} key={user.message_id} />
+                    )
+                  })}
+                </TableBody>
 
-                        <p style={{
-                          fontSize: 12,
-                          fontFamily: 'Open Sans',
-                          letterSpacing: '0.41px',
-                          textAlign: 'right',
-                          color: 'var(--text-light)',
-                          lineHeight: 1.33
-                        }}>
-                          {new Date(user.read_datetime).toLocaleTimeString()}
-                        </p>
-                      </div></td>
-                    </tr>
-                  )
-                })}
-              </tbody>
+              </Table>
+            </TableContainer>
 
-            </table>
+
+
             {report.broadcast.report.length > 1 &&
               <>
                 <p
